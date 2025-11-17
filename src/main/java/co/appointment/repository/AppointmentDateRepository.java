ff9d54f4109993b9b7efe790f6ef33475ee5fa3b
Menelisi Mthembu <menelismthembu@gmail.com>
2025-11-17 21:28:05 +0200

    Removed unused classes

--- src/main/java/co/appointment/repository/AppointmentDateRepository.java
+++
@@ -1,7 +0,0 @@
-package co.appointment.repository;
-
-import co.appointment.entity.AppointmentDate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppointmentDateRepository extends JpaRepository<AppointmentDate, Long> {
-}
--- src/main/java/co/appointment/repository/BranchDailySlotRepository.java
+++
@@ -1,6 +0,0 @@
-package co.appointment.repository;
-
-import co.appointment.entity.BranchDailySlot;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BranchDailySlotRepository extends JpaRepository<BranchDailySlot, Long> {}
