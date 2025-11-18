db42c1c07f3f8f8b1b95954cab1eb47fb212dddd
Menelisi Mthembu <menelismthembu@gmail.com>
2025-11-19 01:56:31 +0200

    Added properties for converting authorities: - authoritiesClaimName - authoritiesPrefix

--- src/main/java/co/appointment/config/AppConfigProperties.java
+++ src/main/java/co/appointment/config/AppConfigProperties.java
@@ -10,4 +10,12 @@ public class AppConfigProperties {
     private OpenApiSettings openApi = new OpenApiSettings();
     private String[] whiteList;
     private String[] adminRoutes = {};
+    /**
+     * Authorities claim name. Default roles.
+     */
+    private String authoritiesClaimName = "roles";
+    /**
+     * Authorities claim prefix. Default ROLE_.
+     */
+    private String authoritiesPrefix = "ROLE_";
 }
