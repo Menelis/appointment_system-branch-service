40d1624ca9f217fd887b6877c3927e3ae9c1e4de
Menelisi Mthembu <menelismthembu@gmail.com>
2025-12-01 01:36:05 +0200

    extend JpaSpecificationExecutor in order to use JPA Specifications

--- src/main/java/co/appointment/repository/BranchRepository.java
+++ src/main/java/co/appointment/repository/BranchRepository.java
@@ -2,9 +2,10 @@ package co.appointment.repository;
 
 import co.appointment.entity.Branch;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface BranchRepository extends JpaRepository<Branch, Integer> {
+public interface BranchRepository extends JpaRepository<Branch, Integer>, JpaSpecificationExecutor<Branch> {
     List<Branch> findAllByProvinceIdAndCityId(Integer provinceId, Integer cityId);
 }
